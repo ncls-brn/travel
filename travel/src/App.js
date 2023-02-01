fbff50ab5162a7e239a34cd5dc43cade3392c140
ncls-brn <nicolas.j.baran@gmail.com>
2023-02-01 15:16:49 +0100

    navbar

---
+++ travel/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+//import { Routes } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <Navbar/>
+      
+     
+
+    </div>
+  );
+}
+
+export default App;
