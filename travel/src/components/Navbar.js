347f3990a0e765fb84424cfdaff181a25bb27fa5
ncls-brn <nicolas.j.baran@gmail.com>
2023-02-02 18:39:30 +0100

    navbar responsive not finish

--- travel/src/components/Navbar.js
+++ travel/src/components/Navbar.js
@@ -17,7 +17,7 @@ class Navbar extends Component {
                     <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
                 </div>
 
-                <ul className="nav-menu">
+                <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
                     {MenuItems.map((item, index) => {
                         return (
                             <li key={index}>
